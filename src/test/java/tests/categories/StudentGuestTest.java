ae01288ddde0e1f00976cb9bb8ef590f2c2cd8a2
Mary Jane Barger <mjb@csatp.com>
2016-09-12 15:40:36 -0700

    committing ART API testing files

--- src/test/java/tests/categories/StudentGuestTest.java
+++
@@ -1,9 +0,0 @@
-package tests.categories;
-
-/**
- * This interface defines a jUnit category for tests executed with a guest student.
- *
- * Created by emunoz on 1/20/16.
- */
-public interface StudentGuestTest {
-}
