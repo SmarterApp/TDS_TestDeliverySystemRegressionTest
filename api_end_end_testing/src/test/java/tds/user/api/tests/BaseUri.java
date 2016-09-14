c2c2ae2dfe4a7cb287eab46d3800eb0b1dc25c4f
Todd Miller <tmiller@fairwaytech.com>
2016-09-14 10:41:07 -0700

    load config file from the filepath

--- api_end_end_testing/src/test/java/tds/user/api/tests/BaseUri.java
+++ api_end_end_testing/src/test/java/tds/user/api/tests/BaseUri.java
@@ -1,8 +1,11 @@
 package tds.user.api.tests;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 
 /**
@@ -13,19 +16,27 @@ public abstract class BaseUri {
     String userUri = null;
 
     @BeforeClass
-    public void init() {
+    public void init() throws IOException {
+        String configFileLocation = System.getProperty("config");
+
+        if (configFileLocation == null || configFileLocation.isEmpty()) {
+            throw new IllegalArgumentException("config file location system property is required (mvn test -Dconfig=<fileLocation>");
+        }
 
         Properties prop = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+        InputStream inputStream = null;
 
         try {
+            inputStream = new FileInputStream(configFileLocation);
             prop.load(inputStream);
             System.out.println("auth prop " + prop.getProperty("authenticateURI"));
+
+            authenticateURI = prop.getProperty("authenticateURI");
+            userUri = prop.getProperty("user.URI");
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
         }
-        catch (IOException e) {
-            e.printStackTrace(System.out);
-        }
-        authenticateURI = prop.getProperty("authenticateURI");
-        userUri = prop.getProperty("user.URI");
     }
 }
