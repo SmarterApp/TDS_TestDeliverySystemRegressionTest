678f150f7100d9dc4bd36f07176649e8e437379a
Mary Jane Barger <mjb@csatp.com>
2016-09-16 13:04:44 -0700

    Restructured code, new methods, added authentication to BaseUri class.

--- api_end_end_testing/src/test/java/tds/user/api/tests/BaseUri.java
+++ api_end_end_testing/src/test/java/tds/user/api/tests/BaseUri.java
@@ -4,16 +4,23 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.UUID;
 
-import org.testng.Assert;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Header;
 import org.testng.annotations.BeforeClass;
 
+import static com.jayway.restassured.RestAssured.given;
+
 /**
- * Created by mjbarger on 9/6/16.
+ * Class to read properites file containing URIs, and authentication specific
+ *    information.
  */
 public abstract class BaseUri {
     String authenticateURI = null;
-    String userUri = null;
+    String userURI = null;
+    String userEmail = null;
+    Header authHeader = null;
 
     @BeforeClass
     public void init() throws IOException {
@@ -29,14 +36,50 @@ public abstract class BaseUri {
         try {
             inputStream = new FileInputStream(configFileLocation);
             prop.load(inputStream);
-            System.out.println("auth prop " + prop.getProperty("authenticateURI"));
 
             authenticateURI = prop.getProperty("authenticateURI");
-            userUri = prop.getProperty("user.URI");
+            userURI = prop.getProperty("userURI");
+            RestAssured.baseURI = userURI;
         } finally {
             if (inputStream != null) {
                 inputStream.close();
             }
         }
+
+        getAuthenticationCode();
+        userEmail = createRandomUser();
+    }
+
+    private void getAuthenticationCode() {
+
+        String accessToken = given()
+            .contentType("application/x-www-form-urlencoded")
+            .queryParam("realm", "/sbac")
+            .formParam("client_id", "pm")
+            .formParam("client_secret", "sbac12345")
+            .formParam("grant_type", "password")
+            .formParam("password", "password")
+            .formParam("username", "prime.user@example.com")
+            .when()
+            .post(authenticateURI + "/auth/oauth2/access_token")
+            .then()
+            .statusCode(200)
+            .extract()
+            .path("access_token");
+
+        authHeader = new Header("Authorization", "Bearer " + accessToken);
+    }
+
+    private String createRandomUser() {
+        String randomStr = UUID.randomUUID().toString();
+        int pos = randomStr.indexOf("-");
+        userEmail = "jennifer.smith" + randomStr.substring(0,pos) + "@example.com";
+        System.out.println("**** userEmail: " + userEmail);
+
+        return userEmail;
+    }
+
+    public String getRandomUser() {
+        return userEmail;
     }
 }
--- api_end_end_testing/src/test/java/tds/user/api/tests/CreateUserTest.java
+++ api_end_end_testing/src/test/java/tds/user/api/tests/CreateUserTest.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.endsWith;
 
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Header;
 import com.jayway.restassured.http.ContentType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.annotations.Test;
@@ -14,128 +14,116 @@ import tds.user.api.model.UserInfo;
 import tds.user.api.model.RoleAssociation;
 
 /*
-    Authenticate user
-    Testing HTTP POST of https://sso-deployment.sbtds.org/auth/oauth2/access_token?realm=/sbac
-    and success status 200
+ * Authenticate user
+ * Testing HTTP POST of https://sso-deployment.sbtds.org/auth/oauth2/access_token?realm=/sbac
+ * and success status 200
  */
 public class CreateUserTest extends BaseUri{
-    String accessToken = null;
-    String email = null;
+    private String uriLocation = "/rest/external/user";
+
+    /*
+     * Test of Create User, HTTP POST of /rest/external/user, 201 success item created
+     * Test Delete User by Email, HTTP DELETE of /rest/external/user, 204 success item
+     * found and deleted
+     */
+    private void createUserOneRoleAssoc() {
+
+        List<RoleAssociation> roleAssociations = new ArrayList<RoleAssociation>();
+        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "44886"));
+
+        UserInfo userInfo = new UserInfo(userEmail, "amy", "watson", "800-332-4747", roleAssociations);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header(authHeader)
+            .body(userInfo)
+        .when()
+            .post(uriLocation)
+        .then()
+            .statusCode(201)
+            .header("location", endsWith(uriLocation + "/" + userEmail + "/details"));
+
+        // Execute a GET to validate
+    }
+
+
+    private void deleteUser() {
+        // Now delete the user that was just created
+        given()
+            .contentType(ContentType.JSON)
+            .header(authHeader)
+            .body(userEmail)
+        .when()
+            .delete(uriLocation)
+        .then()
+            .statusCode(204);
+
+        // Execute a GET to validate user is deleted
+
+    }
+
+    /*
+     * Test of Create User, HTTP POST of /rest/external/user, 201 success item created
+     * Test of Update User, HTTP POST of /rest/external/user, 204 success item updated
+     */
+ //   @Test
+    public void updateUserOneRoleAssoc() {
+        createUserOneRoleAssoc();
+
+        List<RoleAssociation> roleAssociations = new ArrayList<RoleAssociation>();
+        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
+
+        // Update the user with new information
+        UserInfo userInfo = new UserInfo(userEmail, "Bob", "Miller", "858-748-1122", roleAssociations);
+
+        given()
+            .contentType(ContentType.JSON)
+            .header(authHeader)
+            .body(userInfo)
+        .when()
+            .post(uriLocation)
+        .then()
+            .statusCode(204);
+
+     //   deleteUser();
+    }
 
     @Test
-    public void getAuthenticationCode() {
-        RestAssured.baseURI = authenticateURI;
-
-        Header header = new Header("Content-Type", "application/x-www-form-urlencoded");
-
-        accessToken = given()
-            .contentType("application/x-www-form-urlencoded")
-            .header(header)
-            .queryParam("realm", "/sbac")
-            .formParam("client_id", "pm")
-            .formParam("client_secret", "sbac12345")
-            .formParam("grant_type", "password")
-            .formParam("password", "password")
-            .formParam("username", "prime.user@example.com")
-            .when()
-            .post("/auth/oauth2/access_token")
-            .then()
-            .statusCode(200)
-            .extract()
-            .path("access_token");
-    }
-
-    /*
-        Create user
-        Testing HTTP POST of /rest/external/user, 201 success item created
-    */
-    @Test(dependsOnMethods = "getAuthenticationCode")
-    public void createUser() {
-        email = "betsy.ross@example.com";
-
-        RestAssured.baseURI = userUri;
+    public void updateUserMultiRoleAssoc() {
 
         List<RoleAssociation> roleAssociations = new ArrayList<RoleAssociation>();
-        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
-        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
+        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "66283"));
+        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "77392"));
 
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        UserInfo userInfo = new UserInfo(email, "Betsy", "Ross", "800-332-4747", roleAssociations);
-
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonInString = null;
-
-        try {
-            jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(userInfo);
-            System.out.println("jsonInString: " + jsonInString);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Update the user with new information
+        UserInfo userInfo = new UserInfo(userEmail, "Bob", "Miller", "619-222-1122", roleAssociations);
 
         given()
             .contentType(ContentType.JSON)
             .header(authHeader)
             .body(userInfo)
-            .when()
-            .post("/rest/external/user")
-            .then()
-            .statusCode(201);
-    }
-
-    /*
-        Update user
-        Testing HTTP POST of /rest/external/user, 204 success item updated
-    */
-    @Test(dependsOnMethods = "createUser")
-    public void updateUser() {
-
-        RestAssured.baseURI = userUri;
-
-        List<RoleAssociation> roleAssociations = new ArrayList<RoleAssociation>();
-        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
-        roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
-
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        UserInfo userInfo = new UserInfo(email, "Betsy", "Carson", "800-332-4747", roleAssociations);
-
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonInString = null;
-
-        try {
-            jsonInString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(userInfo);
-            System.out.println("jsonInString: " + jsonInString);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        given()
-            .contentType(ContentType.JSON)
-            .header(authHeader)
-            .body(userInfo)
-            .when()
-            .post("/rest/external/user")
-            .then()
+        .when()
+            .post(uriLocation)
+        .then()
             .statusCode(204);
+
+     //   deleteUser();
     }
 
     /*
         Create invalid user
         Testing HTTP POST of /rest/external/user, 400 bad request
     */
-    @Test(dependsOnMethods = "updateUser")
+   // @Test
     public void invalidUser() {
 
-        RestAssured.baseURI = userUri;
+        RestAssured.baseURI = userURI;
 
         List<RoleAssociation> roleAssociations = new ArrayList<RoleAssociation>();
         roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
         roleAssociations.add(new RoleAssociation("Administrator", "CLIENT", "98765"));
 
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        UserInfo userInfo = new UserInfo(email, "Betsy", "", null, roleAssociations);
+        UserInfo userInfo = new UserInfo(userEmail, "Betsy", "", null, roleAssociations);
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonInString = null;
@@ -161,20 +149,16 @@ public class CreateUserTest extends BaseUri{
         Get user by email
         Testing HTTP GET of /rest/external/user/{email}/details, 200 success return student info
     */
-    @Test(dependsOnMethods = "invalidUser")
+  //  @Test
     public void getUserByEmail() {
-        RestAssured.baseURI = userUri;
-
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        System.out.println("endpoint: " + "/rest/external/user/" + email + "/details");
+        RestAssured.baseURI = userURI;
 
         given()
             .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
             .header(authHeader)
             .when()
-            .get("/rest/external/user/" + email + "/details")
+            .get("/rest/external/user/" + userEmail + "/details")
             .then()
             .statusCode(200);
     }
@@ -183,15 +167,11 @@ public class CreateUserTest extends BaseUri{
         Get user by Email that is non-existent
         Testing HTTP GET of /rest/external/user/{email}/details, 404 not found
     */
-    @Test(dependsOnMethods = "invalidUser")
+  //  @Test
     public void getUserByInvalidEmail() {
-        RestAssured.baseURI = userUri;
+        RestAssured.baseURI = userURI;
 
         String invalidEmail = "invalidEmail@example.com";
-
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        System.out.println("endpoint: " + "/rest/external/user/" + email + "/details");
 
         given()
             .contentType(ContentType.JSON)
@@ -203,45 +183,23 @@ public class CreateUserTest extends BaseUri{
             .statusCode(404);
     }
 
-    /*
-        Delete user by Email
-        Testing HTTP DELETE of /rest/external/user, 204 success item found and deleted
-    */
-    @Test(dependsOnMethods = "getUserByInvalidEmail")
-    public void deleteUserByEmail() {
-
-        RestAssured.baseURI = userUri;
-
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
-
-        given()
-            .contentType(ContentType.JSON)
-            .header(authHeader)
-            .body(email)
-            .when()
-            .delete("/rest/external/user")
-            .then()
-            .statusCode(204);
-    }
 
     /*
         Delete user by Email that is non-existent
         Testing HTTP DELETE of /rest/external/user, 404 not found
     */
-    @Test(dependsOnMethods = "deleteUserByEmail")
+    @Test
     public void deleteUserByInvalidEmail() {
-
-        RestAssured.baseURI = userUri;
-
-        Header authHeader = new Header("Authorization", "Bearer " + accessToken);
+        String invalidEmail = "abc123@example.com";
 
         given()
             .contentType(ContentType.JSON)
             .header(authHeader)
-            .body(email)
+            .body(invalidEmail)
             .when()
-            .delete("/rest/external/user")
+            .delete(uriLocation)
             .then()
             .statusCode(404);
     }
+
 }
